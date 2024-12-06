7e54c958a95ab57911bf8f5252efc5e3174a6c0f
Braden Scothern <BradenScothern@gmail.com>
2024-12-05 21:39:34 -0700

    Setup project to use SwiftPM for code instead of the Xcode target

---
+++ Example SwiftPM Project/Anchor.swift
@@ -0,0 +1,8 @@
+//
+//  Anchor.swift
+//  Example SwiftPM Project
+//
+//  Created by Braden Scothern on 12/5/24.
+//
+
+// Empty Anchor file is required to make Xcode happy so it has something to build.
---
+++ Modules/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Modules",
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        .library(
+            name: "App",
+            targets: ["App"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "App"
+        ),
+    ]
+)
