6d0e2268afa86679836c61f07b6639e16984c00b
Braden Scothern <BradenScothern@gmail.com>
2024-12-05 21:33:24 -0700

    Initial Commit

---
+++ Example SwiftPM Project/Example_SwiftPM_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Example_SwiftPM_ProjectApp.swift
+//  Example SwiftPM Project
+//
+//  Created by Braden Scothern on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Example_SwiftPM_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
